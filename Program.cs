e389f0e4e370e8005675ed3238366575deb938b6
DenSad81 <tagil81@mail.ru>
2024-12-19 21:05:51 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -11,18 +11,20 @@ class Program
         int qyantityDetails = 111;
         DetailList detailList = new DetailList(qyantityDetails);
         StoreHouse storeHouse = new StoreHouse(detailList);
+
         CarMaker carMaker = new CarMaker(detailList);
 
         AutoServis autoServis = new AutoServis(storeHouse, carMaker.CreateCars());
+        autoServis.Play();
     }
 }
 
-class AutoServis
+public class AutoServis
 {
     private int _money = 0;
+    private int _fine = 300;
     private StoreHouse _storeHouse;
     private List<Car> _cars;
-
 
     public AutoServis(StoreHouse storeHouse, List<Car> cars)
     {
@@ -30,25 +32,30 @@ class AutoServis
         _cars = cars;
     }
 
-    public void GetIndexOfNededDetail()
+    public void Play()
     {
         while (_cars.Count > 0)
         {
+            this.ShowStats();
+            _cars[0].ShowStats();
+
             if (Utils.ReadBool("Repair car? Y/N"))
             {
                 bool isChangeDetal = true;
+
                 while (isChangeDetal)
                 {
                     Detail detail = _cars[0].GetCopyOfFirstBrokenDetail;
-                    int nededDetailId = detail.Id;
+                    int detailId = detail.Id;
+                    detail.ShowStats();
 
                     if (Utils.ReadBool("Change detal? Y/N"))
                     {
-                        if (_storeHouse.CheckIfDetalPresentById(nededDetailId, out int prisePerDetal, out int prisePerChange))
+                        if (_storeHouse.CheckIfDetalPresentById(detailId, out int prisePerDetal, out int prisePerChange))
                         {
                             if (_cars[0].TruRepairFirstBrokenDetail(prisePerDetal, prisePerChange))
                             {
-                                if (_storeHouse.TryBuyDetalById(nededDetailId))
+                                if (_storeHouse.TryBuyDetalById(detailId))
                                 {
                                     _money += (prisePerDetal + prisePerChange);
 
@@ -74,33 +81,20 @@ class AutoServis
             }
             else
             {
-                int fine = 300;
-                _cars[0].GetFine(fine);
-                _money += fine;
+                _cars[0].GetFine(_fine);
+                _money += _fine;
 
                 _cars.RemoveAt(0);
             }
         }
     }
 
-
-    public void ShowStats()
-    {
-        Console.Clear();
-
-        foreach (var position in _positions)
-            position.ShowStats();
-
-        Console.WriteLine($"Autoservis: {_money}");
-    }
+    private void ShowStats() =>
+        Console.WriteLine($"Ballance {_money}  Quantity car in enqique {_cars.Count}");
 }
 
-class Detail
+public class Detail
 {
-    public int Id { get; /*private set;*/ }
-    public int Prise { get; /*private set;*/ }
-    public int PrisePerChange { get; /*private set;*/ }
-
     public Detail(int type, int prise = 0, int prisePerChange = 0)
     {
         Id = type;
@@ -108,29 +102,27 @@ class Detail
         PrisePerChange = prisePerChange;
     }
 
-    public void ShowStats()
-    {
-        Console.Write($"Type: {Id}  Prise: {Prise}  Prise per change: {PrisePerChange}  ");
-    }
+    public int Id { get; }
+    public int Prise { get; }
+    public int PrisePerChange { get; }
 
-    public Detail Clone()
-    {
-        return new Detail(Id, Prise, PrisePerChange);
-    }
+    public void ShowStats() =>
+        Console.WriteLine($"Type of detail: {Id}  Prise: {Prise}  Prise per change: {PrisePerChange}  ");
+
+    public Detail Clone() =>
+        new Detail(Id, Prise, PrisePerChange);
 }
-class DetailList
+
+public class DetailList
 {
-    private List<Detail> _details;
-    private int _quantityDetails = 99;
+    private List<Detail> _details = new List<Detail>();
 
-    public DetailList(int quantityDetails = 99)
+    public DetailList(int quantityDetails = 99, int minPrice = 50, int maxPrice = 100)
     {
-        _quantityDetails = quantityDetails;
-
-        for (int i = 0; i < _quantityDetails; i++)
+        for (int i = 0; i < quantityDetails; i++)
         {
-            int prise = Utils.GenerateRandomInt(50, 100);
-            int prisePerChange = Utils.GenerateRandomInt(50, 100);
+            int prise = Utils.GenerateRandomInt(minPrice, maxPrice);
+            int prisePerChange = Utils.GenerateRandomInt(minPrice, maxPrice);
 
             _details.Add(new Detail(i, prise, prisePerChange));
         }
@@ -144,72 +136,33 @@ class DetailList
 
     public Detail GetRandomDetail()
     {
-        Detail temp = _details[Utils.GenerateRandomInt(50, _details.Count())];
-        return temp.Clone();
+        Detail detail = _details[Utils.GenerateRandomInt(0, _details.Count())];
+        return detail.Clone();
     }
 
     public List<Detail> GetAllDetails()
     {
-        List<Detail> temp = new List<Detail>();
+        List<Detail> details = new List<Detail>();
 
         foreach (var detail in _details)
-            temp.Add(detail.Clone());
+            details.Add(detail.Clone());
 
-        return temp;
+        return details;
     }
 }
-class StoreHouse
+
+public class StoreHouse
 {
     private List<Detail> _details;
     private List<int> _quantityEachDetails;
-    private int _quantityDetailsPerPosition = 11;
 
-    public StoreHouse(DetailList detailList)
+    public StoreHouse(DetailList detailList, int quantityDetailsPerPosition = 11)
     {
         _details = detailList.GetAllDetails();
+        _quantityEachDetails = new List<int>(_details.Count);
 
         for (int i = 0; i < _details.Count; i++)
-            _quantityEachDetails[i] = _quantityDetailsPerPosition;
-    }
-
-    public bool TryGetCopyDetalById(int id, out Detail detail)
-    {
-        detail = null;
-
-        for (int i = 0; i < _details.Count; i++)
-        {
-            if (_details[i].Id == id)
-            {
-                if (_quantityEachDetails[i] > 0)
-                {
-                    detail = _details[i].Clone();
-                    _quantityEachDetails[i]--;
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public bool TryBuyDetalById(int id, out int prise)
-    {
-        prise = 0;
-
-        for (int i = 0; i < _details.Count; i++)
-        {
-            if (_details[i].Id == id)
-            {
-                if (_quantityEachDetails[i] > 0)
-                {
-                    prise = _details[i].Prise;
-                    _quantityEachDetails[i]--;
-                    return true;
-                }
-            }
-        }
-
-        return false;
+            _quantityEachDetails.Add(quantityDetailsPerPosition);
     }
 
     public bool CheckIfDetalPresentById(int id, out int prisePerDetal, out int prisePerChange)
@@ -243,11 +196,13 @@ class StoreHouse
 
         return false;
     }
-
-
 }
-class CarMaker
+
+public class CarMaker
 {
+    private int _quantityCars = 11;
+    private int _minQuantityBrokenDetailsPerCar = 2;
+    private int _maxQuantityBrokenDetailsPerCar = 6;
     private DetailList _detailList;
 
     public CarMaker(DetailList detailList)
@@ -255,40 +210,40 @@ class CarMaker
         _detailList = detailList;
     }
 
-    private Car CreateCar()
+    public List<Car> CreateCars()
     {
-        int minQuantityBrokenDetails = 3;
-        int maxQuantityBrokenDetails = 6;
-        int quantityDetails = Utils.GenerateRandomInt(minQuantityBrokenDetails, maxQuantityBrokenDetails);
+        List<Car> cars = new List<Car>();
+
+        for (int i = 0; i < _quantityCars; i++)
+            cars.Add(CreateCar(i));
+
+        return cars;
+    }
+
+    private Car CreateCar(int id)
+    {
+        int quantityDetails = Utils.GenerateRandomInt(_minQuantityBrokenDetailsPerCar, _maxQuantityBrokenDetailsPerCar);
         List<Detail> details = new List<Detail>();
 
         for (int i = 0; i < quantityDetails; i++)
             details.Add(_detailList.GetRandomDetail());
 
-        return new Car(details);
-    }
-
-    public List<Car> CreateCars()
-    {
-        int quantityCars = 11;
-        List<Car> cars = new List<Car>();
-
-        for (int i = 0; i < quantityCars; i++)
-            cars.Add(CreateCar());
-
-        return cars;
-    }
+        return new Car(id, details);
+    }    
 }
 
-class Car
+public class Car
 {
     private List<Detail> _brokenDetails;
     private int _money = 1000;
 
-    public Car(List<Detail> details)
+    public Car(int id, List<Detail> details)
     {
+        Id = id;
         _brokenDetails = details;
     }
+
+    public int Id { get; }
 
     public int QuantityBrokenDetails => _brokenDetails.Count;
 
@@ -332,44 +287,26 @@ class Car
 
     public void GetFine(int fine) =>
         _money -= fine;
+
+    public void ShowStats()
+    {
+        Console.WriteLine($"ID car: {Id}  Ballance: {_money}");
+
+        foreach (var brokenDetail in _brokenDetails)
+            brokenDetail.ShowStats();
+    }
 }
 
-
-
-static class Utils
+public static class Utils
 {
     private static Random s_random = new Random();
-
-    public static int ReadInt(string text = "", int minValue = int.MinValue, int maxValue = int.MaxValue)
-    {
-        int number;
-        Console.Write(text + " ");
-
-        while (int.TryParse(Console.ReadLine(), out number) == false || number > maxValue || number < minValue)
-            Console.Write(text + " ");
-
-        return number;
-    }
 
     public static int GenerateRandomInt(int min, int max)
     {
         return s_random.Next(min, max);
     }
 
-    public static bool GenerateRandomBool()
-    {
-        return s_random.Next(2) == 0;
-    }
-
-    static public string ReadString(string text = "")
-    {
-        Console.Write(text + " ");
-        string tempString = Console.ReadLine().ToLower();
-        Console.WriteLine();
-        return tempString;
-    }
-
-    static public bool ReadBool(string text = "", string yes = "y", string no = "n")
+    public static bool ReadBool(string text = "", string yes = "y", string no = "n")
     {
         Console.Write(text + " ");
         bool isOut = false;
@@ -393,124 +330,5 @@ static class Utils
         }
 
         return isOut;
-    }
-
-    static public void ShowArrayString(string[] inputArray)
-    {
-        for (int i = 0; i < inputArray.Length; i++)
-        {
-            Console.WriteLine(inputArray[i]);
-        }
-        Console.WriteLine();
-    }
-
-    static public void ShowDoubleArrayInt(int[,] array)
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int k = 0; k < array.GetLength(1); k++)
-            {
-                Console.Write(array[i, k]);
-                Console.Write(" ");
-            }
-
-            Console.WriteLine();
-        }
-    }
-
-    static public void DeleteLastElementInArrayString(ref string[] inputArray)
-    {
-        if (inputArray.Length > 0)
-        {
-            string[] tempArray = new string[inputArray.Length - 1];
-
-            for (int i = 0; i < tempArray.Length; i++)
-            {
-                tempArray[i] = inputArray[i];
-            }
-
-            inputArray = tempArray;
-        }
-        else
-        {
-            Console.WriteLine("Невозможно удалить то чего нет");
-        }
-    }
-
-    static public int[,] DeleteElementInDoubleArrayInt(int[,] array, int posOfElement)
-    {
-        if (posOfElement >= array.GetLength(0) || posOfElement < 0)
-        {
-            Console.WriteLine("Incorrect index");
-            return null;
-        }
-
-        int[,] arrayTemp = new int[array.GetLength(0) - 1, array.GetLength(1)];
-
-        int posInArray = 0;
-        int posInTempArray = 0;
-
-        while (posInArray < array.GetLength(0))
-        {
-            if (posInArray != posOfElement)
-            {
-                for (int t = 0; t < array.GetLength(1); t++)
-                    arrayTemp[posInTempArray, t] = array[posInArray, t];
-
-                posInTempArray++;
-            }
-
-            posInArray++;
-        }
-
-        return arrayTemp;
-    }
-
-    public static string[,] DeleteElementInDoubleArrayString(string[,] array, int positionOfElement)
-    {
-        if (positionOfElement >= array.GetLength(0) || positionOfElement < 0)
-        {
-            Console.WriteLine("Incorrect index");
-            return null;
-        }
-
-        string[,] arrayNew = new string[array.GetLength(0) - 1, array.GetLength(1)];
-
-        int positionInArray = 0;
-        int positionInNewArray = 0;
-
-        while (positionInArray < array.GetLength(0))
-        {
-            if (positionInArray != positionOfElement)
-            {
-                for (int t = 0; t < array.GetLength(1); t++)
-                    arrayNew[positionInNewArray, t] = array[positionInArray, t];
-
-                positionInNewArray++;
-            }
-
-            positionInArray++;
-        }
-
-        return arrayNew;
-    }
-
-    static public int SerchDataInArrayOfString(string[] inputArray, string inputData)
-    {
-        string tempString;
-        string tempData;
-
-        for (int i = 0; i < inputArray.Length; i++)
-        {
-            tempString = inputArray[i].ToLower();
-            tempData = inputData.ToLower();
-
-            if (tempString.Contains(tempData))
-            {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
